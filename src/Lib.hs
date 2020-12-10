4e55bd67b2ec459fb03c02a9a36b459f8b22a308
iori <matsuhar@gmail.com>
2020-12-10 11:36:47 +0100

    adding some simple function

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,11 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+greet :: String -> IO ()
+greet user = do
+  putStrLn $ "hello " <> user
+
+add :: Int -> Int -> Int
+add x y =
+  x + y
